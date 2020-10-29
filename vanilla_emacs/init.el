6870c34e9f486b5c31554e9ff91201c1b7f4b590
Kyle Barton <kjbarton4@gmail.com>
2020-10-28 23:30:34 -0700

    Move to literate config. File and symbol search. evil-magit.

--- vanilla_emacs/init.el
+++ vanilla_emacs/init.el
@@ -1,27 +1,2 @@
-;; The basics
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-
-(setq user-emacs-directory
-      (file-truename (concat default-directory ".emacs.d/")))
-
-;; Get use-package set up
-(require 'package)
-(setq package-archives `(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
+;; It's all in org now!
 (org-babel-load-file (concat default-directory "configuration.org"))
